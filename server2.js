0030bb81a3d9adef9044989c2a02597bafaeb2ee
bibek <devmail.js@gmail.com>
2025-11-11 15:39:12 +0530

    initial commit

---
+++ server2.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express()
+const PORT = 5000
+let reqCount = 0;
+
+app.use((req, res, next) => {
+    reqCount++
+    console.log(`Total requests so far: ${reqCount}`)
+    next()
+})
+
+app.get('/', (req, res) => {
+    res.send(`Hello! This server(1) has handled ${reqCount} requests so far.`);
+})
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`))
